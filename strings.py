a57f5b1978b5aa17e6d97fa3f49dc518a0f8e6ac
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 22:56:11 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,22 @@
+name = "harmandeep kaur Kaler"
+#length function
+print(len(name))
+#returns the first index where the input occurs
+print(name.find("a"))
+#capitalizes the first letter of the string only
+print(name.capitalize())
+#converts string to upper case
+print(name.upper())
+#converts string to lower case
+print(name.lower())
+#true or false if the string is digit or not
+print(name.isdigit())
+#checks if string is alphabetical
+#will return false if space is there
+print(name.isalpha())
+#count char in string
+print(name.count('a'))
+#replace value with another in a string
+print(name.replace('a', 'o'))
+#display string multiple times
+print(name*3)
---
+++ strslice.py
@@ -0,0 +1,12 @@
+#create a substring by extracting elements from another string
+#using [] or slice()
+#[start: stop: step]
+
+name = "Harmandeep Kaur Kaler"
+first_name = name[:11]
+print(first_name)
+rev = name[::-1]
+funky = name[2:14:2]
+web = "https://google.com"
+sl = slice(7,-4)
+print(website[sl])
