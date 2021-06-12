5773d624158be51c5383b752728d0855a0ddbe1f
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 22:58:07 +0530

    relational operators

---
+++ relational.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: \n"))
+b = int(input("Enter second number: \n"))
+
+print("greater than",a > b)
+print("less than",a < b)
+print("equal to: ",a == b)
+print("not equal to: ",a != b)
+print("greater than equal to: ",a >= b)
+print("less than equal to: ",a <= b)
