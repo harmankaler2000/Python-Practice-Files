059976e6f590664b5169fbf98bdf562f79e5e031
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 23:06:34 +0530

    operator precedence

---
+++ operator_precedence.py
@@ -0,0 +1,35 @@
+#precedence
+a = 10 + 20 * 30
+print(a)
+
+name = "Harman"
+age = 0
+
+if name == "Harman" or name == "Harjot" and age >= 2:
+    print("if executed")
+else:
+    print("not true")
+
+
+# Examples of Operator Associativity
+  
+# Left-right associativity 
+# 100 / 10 * 10 is calculated as  
+# (100 / 10) * 10 and not  
+# as 100 / (10 * 10) 
+print(100 / 10 * 10) 
+    
+# Left-right associativity 
+# 5 - 2 + 3 is calculated as  
+# (5 - 2) + 3 and not  
+# as 5 - (2 + 3) 
+print(5 - 2 + 3) 
+    
+# left-right associativity 
+print(5 - (2 + 3)) 
+    
+# right-left associativity 
+# 2 ** 3 ** 2 is calculated as  
+# 2 ** (3 ** 2) and not  
+# as (2 ** 3) ** 2 
+print(2 ** 3 ** 2)
