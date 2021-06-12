9a50d9973e377cbdb98d2a8e23961b3ad2afb8b2
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 23:02:17 +0530

    math module

---
+++ maths.py
@@ -0,0 +1,10 @@
+import math
+pi = math.pi
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(-pi))
+print(pow(pi,2))
+print(math.sqrt(pi))
+print(max(10, 2, 15))
+print(min(pi, -1 , 19))
