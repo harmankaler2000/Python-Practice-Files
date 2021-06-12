6e34ce5c1c585d02ae0168eeb39141342df3a327
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 22:52:45 +0530

    diff between == and is

---
+++ equal_is.py
@@ -0,0 +1,27 @@
+# == and is are different from each other
+
+list1 = []
+list2 = []
+list3 = list1
+
+if(list1 == list2):
+    print("True")
+else:
+    print("False")
+
+if(list1 is list2):
+    print("True")
+else:
+    print("False")
+
+if(list1 is list3):
+    print("True")
+else:
+    print("False")
+
+list3 = list3 + list2
+
+if(list1 is list3):
+    print("True")
+else:
+    print("False")
