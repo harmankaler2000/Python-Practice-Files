54fd362ee4522f257e9da9ca99ba6c4c4d653a30
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 23:03:08 +0530

    python multiple assignment

---
+++ multi_assignment.py
@@ -0,0 +1,12 @@
+#assigning multiple variables at the same time
+# name = "Harman"
+# age = 21
+# attractive = False
+
+name, age, attractive = "Harman", 21, False
+#assigning same value to all variables
+a1 = a2 = a3 = a4 = 30
+print(name)
+print(age)
+print(attractive)
+print(a1,a2,a3,a4, sep="\n")
