7a87fc1bd2170a01c1fe272726071f31f8590994
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 22:56:25 +0530

    special operators

---
+++ special_operators.py
@@ -0,0 +1,22 @@
+a1 = 3
+b1 = 3
+a2 = 'Hamandeep Kaur Kaler'
+b2 = 'Hamandeep Kaur Kaler'
+a3 = [1,2,3]
+b3 = [1,2,3]
+
+#Identity operator
+print(a1 is not b1)
+print(a2 is b2)
+print(a3 is b3)
+
+#membership operator
+x = 'Harmandeep Kaur Kaler'
+y = {3:'a', 4:'b'}
+
+print('H' in x)
+print('Kaur' not in x)
+print('kaur' not in x)
+
+print(3 in y)
+print('b' in y)
