299ad6e82e1eadd923f4d5881e56bb23757d2e96
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 22:53:55 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,34 @@
+name = "harman"
+print(name)
+#check type
+print(type(name))
+#concatenate two values
+first_name = "Harman"
+last_name = "Kaur"
+full_name = first_name +" "+ last_name
+print("Hello "+ full_name)
+
+age = 21
+print(age)
+age += 1
+print(age)
+age -= 1
+print(age)
+print(type(age))
+#error
+#error = age + full_name
+# TypeError: unsupported operand type(s) for +: 'int' and 'str'
+# error = age + full_name
+#print(error)
+#convert int to string using type casting
+print("Your age is : "+str(age))
+#floating point number
+height = 250.5
+print(height)
+print(type(height))
+print("Your height is: "+str(height)+"cm")
+
+#boolean
+human = False
+print("Are you a human?" + str(human))
+print(type(human))
