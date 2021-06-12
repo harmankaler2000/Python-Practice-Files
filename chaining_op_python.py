2a5992ae33b0371a24fe987681808f40dce2240d
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 22:51:27 +0530

    chaining operator

---
+++ chaining_op_python.py
@@ -0,0 +1,11 @@
+#chaining comparison op
+x = 5
+print(1 < x < 10)
+print(10 < x < 20)
+print(x < 10 < x *10 < 100)
+print(10 > x <= 9)
+print(5 == x > 4)
+
+a,b,c,d,e,f = 0, 5, 12, 0, 15, 15
+exp1 = a <= b < c > d is not e is f
+exp2 = a is d > f is not c
