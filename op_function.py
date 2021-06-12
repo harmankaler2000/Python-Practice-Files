89406043b1270b6eb5a65cce617d241bab0200eb
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 23:05:50 +0530

    built-in operator module

---
+++ op_function.py
@@ -0,0 +1,18 @@
+import operator
+
+a = 4
+b = 3
+
+print("add: ",operator.add(a, b))
+print("sub: ",operator.sub(a, b))
+print("mul: ",operator.mul(a, b))
+print("truediv: ",operator.truediv(a, b))
+print("floor: ",operator.floordiv(a, b))
+print("pow: ",operator.pow(a, b))
+print("mod: ",operator.mod(a, b))
+print("lt: ",operator.lt(a, b))
+print("le: ",operator.le(a, b))
+print("eq: ",operator.eq(a, b))
+print("gt: ",operator.gt(a, b))
+print("ge: ",operator.ge(a, b))
+print("ne: ",operator.ne(a, b))
