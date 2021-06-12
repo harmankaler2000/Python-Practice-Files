5137315500cde67a6c597b615b5bf5069d403ea7
harmandeepkaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2021-06-12 22:51:16 +0530

    bitwise operator

---
+++ bitwise.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: \n"))
+b = int(input("Enter second number: \n"))
+
+print("&: ", a & b)
+print("|: ", a | b)
+print("~: ", ~a)
+print("^: ", a ^ b)
+print(">>: ",a>>2)
+print("<<: ",a<<2)
